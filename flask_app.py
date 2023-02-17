d1937d48345bc64c6520e8ea8be423a4d50d07a0
eqiao13 <you@example.com>
2023-02-17 16:09:53 -0600

    Finish bebemail intergration and added success and fail page

--- flask_app.py
+++ flask_app.py
@@ -16,24 +16,41 @@ def flask_portfolio():
 
 @app.route("/contact_form", methods=["POST", "GET"])
 def contact_form():
+    error = None
     if request.method == "POST":
-        contact_name = request.form.get("fullname")
+        contact_name = request.form.get("name")
         contact_email = request.form.get("email")
         contact_phone = request.form.get("phone")
         contact_message = request.form.get("message")
-        response = requests.post('https://bebemail.edjeware.com/emails', json={"to": os.getenv("WEB_MASTER_EMAIL_ADDRESS")})
-        response.raise_for_status()
 
-        flash(f"{contact_name}, {contact_email}, {contact_phone}, {contact_message}")
-        return redirect(url_for("form_data"))
+        #checking to see if they filled out all the fields in the form
+        if contact_name is "" or contact_email is "" or contact_phone is "" or contact_message is "":
+            return redirect(url_for('form_fail'))
+        else:
+            #connecting to the email server
+            response = requests.post('https://bebemail.edjeware.com/v1/emails', json={"to": os.getenv("WEB_MASTER_EMAIL_ADDRESS"), "subject": "A message for the avatar", "body": f"You got an email from {contact_name}\nEmail: {contact_email}\nPhone number: {contact_phone}\nMessage: {contact_message}"})
+            #breakpoint()
+
+            #flashing message if it send or error and display those messages in a new link HTML page
+            status = response.raise_for_status()
+            if status == 200 or 202:
+                flash(f"You sent\n\n From: {contact_name}\n\n Email: {contact_email}\n\n Phone: {contact_phone}\n\n Message:{contact_message}")
+                return redirect(url_for("form_success"))
+            else:
+                flash("Something went wrong, your email didn't go through.", 'error')
+                return redirect(url_for('form_fail'))
     else:
         return render_template('contact_form.html')
 
 
 
 @app.route("/success")
-def form_data():
+def form_success():
     return render_template('form_success.html')
+
+@app.route("/failed")
+def form_fail():
+    return render_template('form_fail.html')
 
 @app.route('/<name>')
 def sleeper(name):
