dc52d0d699afa4079073787add1cadee5cef827b
eveqiao13 <you@example.com>
2023-02-17 00:13:46 +0000

    added contact form bebemail

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, request, url_for, redirect, render_template, flash
+import requests
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def home():
@@ -9,6 +13,27 @@ def home():
 @app.route("/flask_portfolio")
 def flask_portfolio():
     return render_template('portfolio.html')
+
+@app.route("/contact_form", methods=["POST", "GET"])
+def contact_form():
+    if request.method == "POST":
+        contact_name = request.form.get("fullname")
+        contact_email = request.form.get("email")
+        contact_phone = request.form.get("phone")
+        contact_message = request.form.get("message")
+        response = requests.post('https://bebemail.edjeware.com/emails', json={"to": os.getenv("WEB_MASTER_EMAIL_ADDRESS")})
+        response.raise_for_status()
+
+        flash(f"{contact_name}, {contact_email}, {contact_phone}, {contact_message}")
+        return redirect(url_for("form_data"))
+    else:
+        return render_template('contact_form.html')
+
+
+
+@app.route("/success")
+def form_data():
+    return render_template('form_success.html')
 
 @app.route('/<name>')
 def sleeper(name):
@@ -22,5 +47,11 @@ def admin(name=None):
 def sleep(name='eve'):
     return render_template('sleeping.html', name=name)
 
-#app.run(port=5001)
-#http://127.0.0.1:5001
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+#http://127.0.0.1:5000/
+
+
+#{{ url_for('static', filename='')}}
